0920e13e27da6250ff0f211627442af339e2d276
glebe <glevse@yandex.ru>
2024-06-16 09:43:14 +0300

    Restarting the git profile

---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/Basement/Camera.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Camera.Basement;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class Camera {
+    VisionPortal visionPortal;
+    AprilTagProcessor aprilTagProcessor;
+
+    public class OpenCvOld{
+
+    }
+    public class OpenCVnew{
+
+    }
+    public class AprilTagOld{
+
+    }
+    public class TensorFLowold{
+
+    }
+
+}
