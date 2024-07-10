bddf1e78a43e391e4fd6ee673ecaa54d67e778f0
glebe <glevse@yandex.ru>
2024-07-10 22:00:40 +0300

    writing control theory

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Robot.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Robot.java
@@ -33,30 +33,7 @@ public class Robot {
         phantomIMU.initIMU(hardwareMap);
     }
 
-    public void teleopMovement(Gamepad gamepad1, Gamepad gamepad2){
-        Thread math = new Thread(() ->{
-            while (true){
-                if (gamepad1.right_bumper) {
-                    rBumper = 0.4;
-                }
-                if (gamepad1.left_bumper){
-                    lBumper = 0.4;
-                }
-                forwardSpeed = gamepad1.left_stick_y + gamepad1.right_stick_y * 0.4;
-                strafeSpeed = gamepad1.left_stick_x + gamepad1.right_stick_x * 0.4;
-                turnSpeed = (gamepad1.right_trigger - gamepad1.left_trigger) + (rBumper - lBumper);
-            }
-        });
-        Thread teleopMovement = new Thread(() -> {
-           wheelBase.mecanumDrive.driveRobotCentric(
-                strafeSpeed,
-                   forwardSpeed,
-                   turnSpeed
-           );
-        });
-        math.start();
-        teleopMovement.start();
-    }
+
     public class Position{
         public double x, y, heading;
         public Position(double x, double y, double heading) {
