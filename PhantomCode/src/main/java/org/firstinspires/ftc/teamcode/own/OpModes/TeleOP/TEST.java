bbd031d914b359cb64d83f90523aded0c6270c63
Bloxorer <phantom16950@gmail.com>
2024-10-01 16:02:55 +0300

    Working with controllers

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/OpModes/TeleOP/TEST.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/OpModes/TeleOP/TEST.java
@@ -7,18 +7,31 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.own.Utils.Controllers.KalmanFilter;
+
 @TeleOp
 public class TEST extends LinearOpMode {
     ElapsedTime time = new ElapsedTime();
+    KalmanFilter filter;
+    double filteredPos;
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "test");
+        filter = new KalmanFilter(0.3, 0.3, motor.getCurrentPosition(), motor.getTargetPosition());
         time.reset();
         waitForStart();
         while(opModeIsActive()){
+           filteredPos = filter.calculate();
+            telemetry.addData("filteredPos", filteredPos);
+            telemetry.addData("time", time.milliseconds());
             motor.setPower(0.1);
-            wait(1000);
+            sleep(1000);
             motor.setPower(0);
+            sleep(1000);
+            motor.setPower(-0.1);
+            sleep(1000);
+            motor.setPower(0);
+            telemetry.update();
         }
     }
 }
