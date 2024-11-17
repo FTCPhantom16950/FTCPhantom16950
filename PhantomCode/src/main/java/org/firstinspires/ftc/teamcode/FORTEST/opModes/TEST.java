f722cedb35ed00f68e3c23e478feacbd204eb577
Bloxorer <phantom16950@gmail.com>
2024-11-17 16:49:07 +0300

    Gradle update

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/FORTEST/opModes/TEST.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/FORTEST/opModes/TEST.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.own.Mechanism.LynxModule;
 import org.firstinspires.ftc.teamcode.own.Mechanism.WheelBase;
 import org.firstinspires.ftc.teamcode.own.Utils.PhantomIMU;
 
--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/FORTEST/opModes/TEST1.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/FORTEST/opModes/TEST1.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.FORTEST.opModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.own.Mechanism.HorizontSlider;
