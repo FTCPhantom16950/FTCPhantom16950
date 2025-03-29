591eb808f07532294ad23d0c1f1a66bcf3bc7b21
Bloxorer <phantom16950@gmail.com>
2025-03-29 13:43:47 +0300

    Action in use

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/PhMath.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/PhMath.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.own.Utils;
 
-import androidx.annotation.Nullable;
-
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.FORTEST.CustomExcpection;
@@ -10,23 +8,22 @@ public class PhMath extends Thread {
     @Override
     public synchronized void start() {
         super.start();
-
     }
 
-    public static double fromDegreesToPower(double power, double max){
-        if(power > max || power < 0){
+    public static double fromDegreesToPower(double power, double max) {
+        if (power > max || power < 0) {
             throw new CustomExcpection("Значение угла сервы: " + power + "больше чем максимальный угол: " + max);
         }
-        double itogpower = 0;
-        if(power/ (max / 2) == 0) {
-            itogpower = -1;
-        }else if (power / (max / 2) == 1) {
-            itogpower = 0;
-        } else if (power/ (max / 2) == 2) {
-            itogpower = 1;
-        } else{
-            itogpower = Range.clip(power / (max / 2) - 1 , -1, 1);
+        double resultPower = 0;
+        if (power / (max / 2) == 0) {
+            resultPower = -1;
+        } else if (power / (max / 2) == 1) {
+            resultPower = 0;
+        } else if (power / (max / 2) == 2) {
+            resultPower = 1;
+        } else {
+            resultPower = Range.clip(power / (max / 2) - 1, -1, 1);
         }
-        return itogpower;
+        return resultPower;
     }
 }
