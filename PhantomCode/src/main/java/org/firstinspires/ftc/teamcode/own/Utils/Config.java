cd932c0095e0cb33690e73d2f56bd3c037b88770
Bloxorer <phantom16950@gmail.com>
2025-03-29 15:04:16 +0300

    Config.java refactor

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Config.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Config.java
@@ -17,17 +17,29 @@ package org.firstinspires.ftc.teamcode.own.Utils;
        |_____-ls                  -rs_____|
  */
 
-@com.acmerobotics.dashboard.config.Config
 /**
  * класс конфига для настройки из FTCDashboard
  */
+@com.acmerobotics.dashboard.config.Config
 public class Config {
-    /// включает движение в Телеопе относительно центра поля
+    /**
+     * включает движение в Телеопе относительно центра поля
+     * Default: false
+     */
     public static boolean teleOpIMU = false;
-    /// включает движение в Телеопе при помощи библиотеки PedroPathing
+    /**
+     * включает движение в Телеопе при помощи библиотеки PedroPathing
+     * Default: false
+     */
     public static boolean pedroTeleOp = false;
-    /// хранит выполняется ли действие в данный момент времени
+    /**
+     * хранит выполняется ли действие в данный момент времени
+     * Default: false
+     */
     public static boolean actionInWork = false;
-    /// погрешность для передвижения в автономном периоде (в дюймах)
+    /**
+     * погрешность для передвижения в автономном периоде (в дюймах)
+     * Default: false
+     */
     public static double tolerance = 0.7;
 }
