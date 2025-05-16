e79fb9ce2c970ff843df707847a41f63116941f4
Bloxorer <phantom16950@gmail.com>
2025-05-16 19:54:57 +0300

    создание собственного планировщика действий

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/FORTEST/CustomException.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.FORTEST;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
