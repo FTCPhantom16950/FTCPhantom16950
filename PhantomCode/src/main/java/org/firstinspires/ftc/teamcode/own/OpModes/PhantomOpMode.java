36afec813c18f80e843e14dccb668cda0257c15f
Bloxorer <phantom16950@gmail.com>
2025-05-21 15:34:43 +0300

    создание собственного планировщика действий

---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/OpModes/PhantomOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.own.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.own.Actions.PedroAction;
+import org.firstinspires.ftc.teamcode.own.Utils.Actions;
+
+public class PhantomOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
