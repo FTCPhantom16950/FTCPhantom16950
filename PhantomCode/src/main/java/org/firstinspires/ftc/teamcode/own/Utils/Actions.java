5ea36ca1304ea7d96bfd6b7b8c7f0bbd7f297c2c
Bloxorer <phantom16950@gmail.com>
2025-05-26 22:43:49 +0300

    создание собственного планировщика действий

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Actions.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Actions.java
@@ -3,4 +3,21 @@ package org.firstinspires.ftc.teamcode.own.Utils;
 public abstract class Actions {
     public abstract void play();
 
+    public boolean sleep(long ms) {
+        try {
+            wait(ms);
+        } catch (InterruptedException e) {
+            throw new CustomException("stopped while sleep");
+        }
+        return true;
+    }
+
+    public boolean sleep(int ms) {
+        try {
+            wait(ms);
+        } catch (InterruptedException e) {
+            throw new CustomException("stopped while sleep");
+        }
+        return true;
+    }
 }
---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/CustomException.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.own.Utils;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
