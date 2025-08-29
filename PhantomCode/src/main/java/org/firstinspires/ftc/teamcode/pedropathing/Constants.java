8f61d56b9a3d52fe709f824f3e506f111817c54f
FTCPhantom16950 <79818770327@yandex.com>
2025-08-29 12:55:50 +0300

    tested in school

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.teamcode.pedropathing;
 
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
+import com.bylazar.configurables.PanelsConfigurables;
+import com.bylazar.field.FieldManager;
+import com.bylazar.field.PanelsField;
+import com.bylazar.field.Style;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.follower.Follower;
@@ -16,29 +24,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(16.2)
-            .forwardZeroPowerAcceleration(-25.9346931313679598)
-            .lateralZeroPowerAcceleration(-67.342491844080064)
-            .translationalPIDFCoefficients(new PIDFCoefficients(0.03, 0, 0, 0.015))
-            .translationalPIDFSwitch(4)
-            .secondaryTranslationalPIDFCoefficients(
-                    new PIDFCoefficients(0.4, 0, 0.03, 0.0006)
-            )
-            .headingPIDFCoefficients(new PIDFCoefficients(0.8, 0, 0, 0.01))
-            .secondaryHeadingPIDFCoefficients(
-                    new PIDFCoefficients(2.5, 0, 0.1, 0.0005)
-            )
-            .drivePIDFCoefficients(
-                    new FilteredPIDFCoefficients(0.02, 0.000008, 0.0015, 0.6, 0.01)
-            )
-            .secondaryDrivePIDFCoefficients(
-                    new FilteredPIDFCoefficients(0.05, 0, 0.0004, 0.6, 0.01)
-            )
-            .centripetalScaling(0.0005);
+            .mass(12)
+            .forwardZeroPowerAcceleration(-34.90168374138196)
+            .lateralZeroPowerAcceleration(-92.07477745019709)
+            .centripetalScaling(0.0001)
+            .automaticHoldEnd(true);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1)
             .leftFrontMotorName("lf")
             .leftRearMotorName("lb")
             .rightFrontMotorName("rf")
@@ -46,29 +40,38 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(73.13207972253893)
+            .yVelocity(50.607968786099335);
 
-    public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(.001989436789)
-            .strafeTicksToInches(.001989436789)
-            .turnTicksToInches(.001989436789)
-            .leftPodY(1)
-            .rightPodY(-1)
-            .strafePodX(-2.5)
-            .leftEncoder_HardwareMapName("lf")
-            .rightEncoder_HardwareMapName("rb")
-            .strafeEncoder_HardwareMapName("rf")
-            .leftEncoderDirection(Encoder.FORWARD)
-            .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD)
-            .IMU_HardwareMapName("imu")
-            .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+    public static ThreeWheelIMUConstants localizerConstants =
+            new ThreeWheelIMUConstants()
+                    .forwardTicksToInches(0.003)
+                    .strafeTicksToInches(0.0029)
+                    .turnTicksToInches(0.0027)
+                    .leftPodY(2.8)
+                    .rightPodY(-2.8)
+                    .strafePodX(-4.9)
+                    .leftEncoder_HardwareMapName("rf")
+                    .rightEncoder_HardwareMapName("lb")
+                    .strafeEncoder_HardwareMapName("lf")
+                    .leftEncoderDirection(Encoder.FORWARD)
+                    .rightEncoderDirection(Encoder.REVERSE)
+                    .strafeEncoderDirection(Encoder.REVERSE)
+                    .IMU_HardwareMapName("imu")
+                    .IMU_Orientation(
+                            new RevHubOrientationOnRobot(
+                                    RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                                    RevHubOrientationOnRobot.UsbFacingDirection.UP
+                            )
+                    );
+
     public static PathConstraints pathConstraints = new PathConstraints(
             0.995,
             0.1,
             0.1,
             0.007,
-            100,
+            300,
             4,
             10,
             1
