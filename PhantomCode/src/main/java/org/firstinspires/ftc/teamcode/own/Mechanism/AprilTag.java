508ff3ab922b6e66163d010c2eb6e7dee5fb18b9
glevse <glevse@yandex.ru>
2025-09-10 02:30:50 +0300

    started working with camera

---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Mechanism/AprilTag.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.own.Mechanism;
+
+import org.firstinspires.ftc.teamcode.own.Utils.Mechanism;
+
+public class AprilTag implements Mechanism {
+    @Override
+    public boolean init() {
+        return true;
+    }
+}
---
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/camera/ArtifactProcessor.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.own.camera;
+
+import android.graphics.Canvas;
+
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtifactProcessor implements VisionProcessor {
+    Telemetry telemetry;
+    public static double thr_value = 100, max = 255;
+    public static int type = 1;
+    Mat output, gray, hsv, blur;
+    public static int top, bottom, left, right;
+
+    public ArtifactProcessor(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    Mat mat;
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        width = 1280;
+        height = 720;
+//        output = new Mat();
+        gray = new Mat();
+        hsv = new Mat();
+        blur = new Mat();
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        output = frame;
+//        Imgproc.cvtColor(frame, output, Imgproc.COLOR_BGR2HSV);
+//        Core.inRange(output, low, high, output);
+//        Imgproc.GaussianBlur( frame, blur, new Size(31, 31), 0, 0, Core.BORDER_DEFAULT );
+//        Core.copyMakeBorder(frame, output, (int) (0.05 * frame.rows()), (int) (0.05 * frame.rows()), (int) (0.05 * frame.cols()), (int) (0.05 * frame.cols()), Core.BORDER_CONSTANT, new Scalar(255, 255, 255));
+//  median - very slow blur norm
+//        Imgproc.blur(frame, frame, new Size(5, 5), new Point(-1, -1));
+        Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+//            Imgproc.stackBlur(frame, output, new Size(8,8));
+        Imgproc.threshold(gray, output, 100, 255, 1);
+//        Imgproc.adaptiveThreshold(gray,output,100, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 21, 3);
+        return frame;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        top = (int) (onscreenWidth * scaleCanvasDensity);
+        bottom = (int) (onscreenWidth * scaleCanvasDensity);
+        left = (int) (onscreenHeight * scaleCanvasDensity);
+        right = (int) (onscreenHeight * scaleCanvasDensity);
+    }
+}
