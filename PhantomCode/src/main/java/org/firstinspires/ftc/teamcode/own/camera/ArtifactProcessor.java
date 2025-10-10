362366c0c79e6c7ba7b8211e5ca95c5ccdaf0480
FTCPhantom16950 <phantom16950@gmail.com>
2025-10-10 19:54:33 +0300

    updated to 11.0

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/camera/ArtifactProcessor.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/camera/ArtifactProcessor.java
@@ -22,6 +22,11 @@ import java.util.List;
 
 @Config
 public class ArtifactProcessor implements VisionProcessor {
+    float c = 4f;
+    float razmer = 51f;
+    float f;
+    float convers;
+    float h;
     List<MatOfPoint> contours = new ArrayList<>();
     Mat hierarchy = new Mat();
     Mat blurredImage = new Mat();
@@ -30,24 +35,25 @@ public class ArtifactProcessor implements VisionProcessor {
     Mat morphOutput = new Mat();
     Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilateElementWidth, dilateElementHeight));
     Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erodeElementWidth, erodeElementHeight));
-    public static int hLow = 7,sLow = 80,vLow = 80,
-    hHigh = 10, sHigh = 255, vHigh = 255,
-    widthBlur = 1, heightBlur = 1, dilateElementWidth = 50, dilateElementHeight = 50,
-            erodeElementWidth = 30,erodeElementHeight = 30;
+    public static int hLow = 7, sLow = 80, vLow = 80,
+            hHigh = 10, sHigh = 255, vHigh = 255,
+            widthBlur = 1, heightBlur = 1, dilateElementWidth = 50, dilateElementHeight = 50,
+            erodeElementWidth = 30, erodeElementHeight = 30;
     Scalar minValues = new Scalar(hLow, sLow, vLow);
-    Scalar maxValues  = new Scalar(hHigh, sHigh,vHigh);
-    Mat output;
+    Scalar maxValues = new Scalar(hHigh, sHigh, vHigh);
     MatOfPoint2f[] contoursPoly;
     Telemetry telemetry;
     Rect[] rects;
-    float scale, bnmToPx;
     public static int minSquare = 0;
+
     public ArtifactProcessor(Telemetry telemetry) {
         this.telemetry = telemetry;
     }
+
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
-
+        f = calibration.focalLengthX;
+        convers = c / 960;
     }
 
     @Override
@@ -62,30 +68,30 @@ public class ArtifactProcessor implements VisionProcessor {
         Imgproc.dilate(mask, morphOutput, dilateElement);
         Imgproc.dilate(mask, morphOutput, dilateElement);
         Imgproc.findContours(morphOutput, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-//        morphOutput = frame;
         contoursPoly = new MatOfPoint2f[contours.size()];
         rects = new Rect[contours.size()];
-        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
-            // for each contour, display it in blue
-            for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
-                Imgproc.drawContours(output, contours, idx, new Scalar(250, 0, 0));
-            }
-        }
-        for (int idx = 0; idx < contours.size(); idx++){
-            if (contours.get(idx).toArray() != null){
+        for (int idx = 0; idx < contours.size(); idx++) {
+            if (contours.get(idx).toArray() != null) {
                 contoursPoly[idx] = new MatOfPoint2f();
                 Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(idx).toArray()), contoursPoly[idx], 3, true);
                 rects[idx] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[idx].toArray()));
+                double a = (-rects[idx].tl().x + rects[idx].br().x),
+                        b = (-rects[idx].tl().y + rects[idx].br().y);
+                double otn = a / b;
+                double square = a * b;
+                if (square >= minSquare && (otn < 1.1) && (otn > 0.95)) {
+                    h = (float) (b * convers);
+                    float rectSizeOnCamera = (float) ((f * razmer) / h);
 
-                double a = rects[idx].tl().x -  rects[idx].br().x,
-                        b = rects[idx].tl().y -  rects[idx].br().y;
-                telemetry.addData("a", a);
-                telemetry.addData("b", b);
-                double otn = a/b;
-                double square = a * b * scale * scale;
-                telemetry.addData("square", square);
-                if (square * scale * scale >= minSquare && (otn < 1.2) && (otn > 0.8)){
-                    Imgproc.rectangle(output, rects[idx].tl(), rects[idx].br(), new Scalar(255,0,0),2);
+                    telemetry.addData("rectSizeOnCamera", rectSizeOnCamera);
+
+                    telemetry.addData("otn", otn);
+                    telemetry.addData("square", square);
+                    telemetry.addData("focal", f);
+                    telemetry.addData("size on camera", convers);
+                    telemetry.addData("a", a);
+                    telemetry.addData("b", b);
+                    Imgproc.rectangle(frame, rects[idx].tl(), rects[idx].br(), new Scalar(255, 0, 0), 2);
                 }
             }
         }
@@ -94,7 +100,6 @@ public class ArtifactProcessor implements VisionProcessor {
         hierarchy.empty();
         morphOutput.empty();
         mask.empty();
-        output = frame;
 //        morphOutput = frame;
         return null;
     }
@@ -102,10 +107,9 @@ public class ArtifactProcessor implements VisionProcessor {
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-    scale = scaleCanvasDensity;
-    bnmToPx = scaleBmpPxToCanvasPx;
-    telemetry.addData("scale",scale);
-    telemetry.addData("bnmToPx",bnmToPx );
+//        telemetry.addData("bnmToPx", pxToMM);
+
+
     }
 
 }
