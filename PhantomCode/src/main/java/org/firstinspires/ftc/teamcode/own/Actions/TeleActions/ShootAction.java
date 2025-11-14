75ef9c73d88f3b0c167cc0f724d24b289a3f84d1
FTCPhantom16950 <phantom16950@gmail.com>
2025-11-15 02:57:27 +0300

    feat: Implement PID control for shooter motor
    Introduced a PID controller to manage the shooter motor's velocity. This replaces the previous manual power adjustment with a closed-loop system for more precise speed regulation.
    
    Key changes:
    - Created a `PIDCofficients` class to store P, I, and D gains.
    - Implemented a generic `PIDController` class, which runs in a separate thread to continuously calculate the required output.
    - Refactored `ShootAction` to use the new `PIDController`, allowing the target velocity to be adjusted via the gamepad.
    - Made PID gains (`kP`, `ki`, `kd`) and `targetVelocity` in `ShootAction` configurable.
    - Added telemetry for shooter power and velocity.

--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Actions/TeleActions/ShootAction.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Actions/TeleActions/ShootAction.java
@@ -1,28 +1,40 @@
 package org.firstinspires.ftc.teamcode.own.Actions.TeleActions;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.own.Utils.Action.Action;
 import org.firstinspires.ftc.teamcode.own.Utils.PhantomOpMode;
+import org.firstinspires.ftc.teamcode.own.Utils.Regulators.PIDCofficients;
+import org.firstinspires.ftc.teamcode.own.Utils.Regulators.PIDController;
 import org.firstinspires.ftc.teamcode.own.Utils.Robot;
-
+@Configurable
 public class ShootAction extends Action {
+    public static double kP = 1, ki = 0, kd = 0, targetVelocity = 0;
+    PIDCofficients pidCofficients = new PIDCofficients(kP,ki,kd);
+    PIDController pidController = new PIDController(pidCofficients);
+
     @Override
     public void execute() {
-        double power = 1;
         DcMotorEx shootMotor = Robot.get("shoot", DcMotorEx.class);
+        pidController.start();
         while (Robot.opMode.opModeIsActive()){
+            pidCofficients.setkP(kP);
+            pidCofficients.setkP(kd);
+            pidController.setPidCofficients(pidCofficients);
+            pidController.setCurrentError(targetVelocity - shootMotor.getVelocity());
             if (Robot.gamepadDriver.y){
-                power += 0.1;
+                targetVelocity += 1000;
                 Robot.opMode.sleep(300);
             }
             else if (Robot.gamepadDriver.a){
-                power += 0.1;
+                targetVelocity -= 1000;
                 Robot.opMode.sleep(300);
             }
-            shootMotor.setPower(power);
-            PhantomOpMode.addData("shootPower", power);
+            shootMotor.setPower(pidController.getOutput());
+            PhantomOpMode.addData("shootPower", shootMotor.getPower());
+            PhantomOpMode.addData("shootVelocity", shootMotor.getVelocity());
         }
     }
 }
--- PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Regulators/PIDController.java
+++ PhantomCode/src/main/java/org/firstinspires/ftc/teamcode/own/Utils/Regulators/PIDController.java
@@ -1,17 +1,44 @@
 package org.firstinspires.ftc.teamcode.own.Utils.Regulators;
 
+import static org.firstinspires.ftc.teamcode.own.Utils.Robot.opMode;
 import static org.firstinspires.ftc.teamcode.own.Utils.Robot.voltageSensor;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-public class PIDController {
-    private double kP = 0, kI = 0, kD = 0;
+public class PIDController extends Thread {
+    public PIDController(PIDCofficients pidCofficients) {
+        this.pidCofficients = pidCofficients;
+    }
+
+    private PIDCofficients pidCofficients;
+    private double currentError = 0d;
+    private double kP, kI, kD;
     ElapsedTime timer = new ElapsedTime();
     private double previousError = 0, previousTime = 0, integralSum = 0;
-    public double update(double currentError) {
+    private double output = 0;
+    public PIDCofficients getPidCofficients() {
+        return pidCofficients;
+    }
+
+    public void setPidCofficients(PIDCofficients pidCofficients) {
+        this.pidCofficients = pidCofficients;
+    }
+
+    public double getCurrentError() {
+        return currentError;
+    }
+
+    public void setCurrentError(double currentError) {
+        this.currentError = currentError;
+    }
+
+    public double update() {
+        kP = pidCofficients.getkP();
+        kI = pidCofficients.getkI();
+        kD = pidCofficients.getkD();
         double time = timer.seconds();
-        double P,I,D;
+        double P, I, D;
         double dE = currentError - previousError;
         double dT;
         if (previousTime == 0) {
@@ -33,6 +60,22 @@ public class PIDController {
         previousError = currentError;
         previousTime = time;
         timer.reset();
-        return Range.clip(P+I+D,-1,1);
+        return Range.clip(P + I + D, -1, 1);
+    }
+
+    public double getOutput() {
+        return output;
+    }
+
+    public void setOutput(double output) {
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (!opMode.isStopRequested()){
+            output = update();
+        }
     }
 }
